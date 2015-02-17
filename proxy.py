eb9d0b9b78babf352f9b5f72a9a911d1cde0b7cf
Robert Helmer <rhelmer@rhelmer.org>
2015-02-17 15:04:35 -0800

    .

---
+++ proxy.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import boto
+from boto.s3.key import Key
+import collections
+import hashlib
+import os
+import cPickle as pickle
+from wsgiref.simple_server import make_server
+
+PORT=8000
+
+class LRUCache:
+    def __init__(self, capacity, cache_dir):
+        self.capacity = capacity
+        self.cache = collections.OrderedDict()
+        self.cache_dir = cache_dir
+        self.index_file = os.path.join(self.cache_dir, 'index')
+        if os.path.exists(self.index_file):
+            self.cache = pickle.load(open(self.index_file, 'r'))
+
+    def __getitem__(self, key):
+        try:
+            self.cache.pop(key)
+            self.cache[key] = 1
+            with open(os.path.join(self.cache_dir, key), 'r') as cachefile:
+                return cachefile.read()
+        except KeyError:
+            return -1
+
+    def __setitem__(self, key, value):
+        if len(self.cache) >= self.capacity:
+            cache_key, file_path = self.cache.popitem(last=False)
+            os.remove(os.path.join(self.cache_dir, cache_key))
+            print 'evict'
+        self.cache[key] = 1
+        with open(os.path.join(self.cache_dir, key), 'w') as cachefile:
+            cachefile.write(value)
+        pickle.dump(self.cache, open(self.index_file, 'w'))
+
+    def __contains__(self, key):
+        print self.cache.keys
+        return key in self.cache
+
+cache = LRUCache(capacity=10, cache_dir='/tmp/cache')
+
+def proxy_s3_bucket(environ, start_response):
+    """proxy private s3 buckets"""
+    path_info = environ.get('PATH_INFO', '')
+    path_info = path_info.lstrip('/')
+    (bucket, key) = path_info.split('/', 1)
+    s3_result = fetch_s3_object(bucket, key)
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return [s3_result]
+
+def fetch_s3_object(bucket, key):
+    m = hashlib.md5()
+    m.update(bucket+key)
+    cache_key = m.hexdigest()
+
+    conn = boto.connect_s3()
+    if cache_key in cache:
+        print 'debug cache hit'
+        return cache[cache_key]
+    else:
+        print 'debug cache miss'
+
+    b = conn.get_bucket(bucket)
+    k = b.get_key(key)
+    obj = k.get_contents_as_string()
+    cache[cache_key] = obj
+    return obj
+
+if __name__ == '__main__':
+    httpd = make_server('', PORT, proxy_s3_bucket)
+    print 'Serving HTTP on port %s...' % PORT
+    httpd.serve_forever()
