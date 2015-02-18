cf8ec94b4ede633f06e48741afa58642ffe9397a
Robert Helmer <rhelmer@rhelmer.org>
2015-02-18 13:21:51 -0800

    make wsgi app honor vars too

--- proxy/wsgi.py
+++ proxy/wsgi.py
@@ -1,5 +1,9 @@
+import os
+import tempfile
 from proxy import CachingS3Proxy
 
 def application(environ, start_response):
-    p = CachingS3Proxy()
+    capacity = int(os.environ.get('CAPACITY', 1000000000))
+    cache_dir = os.environ.get('CACHEDIR', tempfile.gettempdir())
+    p = CachingS3Proxy(capacity, cache_dir)
     return p.proxy_s3_bucket(environ, start_response)
