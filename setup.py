f7992d846c86f5c6794f11293f02cd9ceb478770
Robert Helmer <rhelmer@rhelmer.org>
2015-02-23 15:28:28 -0800

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='caching-s3-proxy',
-    version='0.1',
+    version='0.2',
     description=('provides an unauthenticated plain HTTP frontend for'
                  ' public and private S3 buckets, and caches on the'
                  ' filesystem using an LRU cache'),
