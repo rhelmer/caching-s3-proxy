a700141023e0d7e73ef550eabe7afedbadb793eb
Robert Helmer <rhelmer@rhelmer.org>
2015-02-22 20:23:40 -0800

    remove lockfile if it exists on startup

--- proxy/cache.py
+++ proxy/cache.py
@@ -27,6 +27,8 @@ class LRUCache(object):
         if not os.path.exists(self.cache_dir):
             os.mkdir(self.cache_dir)
         self.lock_path = os.path.join(self.cache_dir, 'lock')
+        if os.path.exists(self.lock_path):
+            os.unlink(self.lock_path)
         self.logger.info('using cache dir %s' % self.cache_dir)
         self.index_file = os.path.join(self.cache_dir, 'index')
         with flock(self.lock_path):
