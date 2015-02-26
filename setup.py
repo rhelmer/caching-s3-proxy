9c1fcce02578c5619e517e94b748d3de09d2f9b7
Robert Helmer <rhelmer@rhelmer.org>
2015-02-25 16:44:31 -0800

    correct ordereddict version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='caching-s3-proxy',
-    version='0.4',
+    version='0.5',
     description=('provides an unauthenticated plain HTTP frontend for'
                  ' public and private S3 buckets, and caches on the'
                  ' filesystem using an LRU cache'),
@@ -23,7 +23,7 @@ setup(
         ],
     packages=['proxy'],
     keywords=['caching', 'lru', 's3', 'proxy', 'unauthenticated'],
-    install_requires=['boto>=2.36.0', 'wsgiref>=0.1.2', 'ordereddict>=1.1.2'],
+    install_requires=['boto>=2.36.0', 'wsgiref>=0.1.2', 'ordereddict>=1.1'],
     entry_points={
         'console_scripts': [
             'caching-s3-proxy = proxy.run:main'
